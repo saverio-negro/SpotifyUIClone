b3a7d4237cd55c8fe33976b4e79c0931d7fb8231
saverio-negro <saverionegro@gmail.com>
2026-03-30 01:01:38 +0200

    [Feature] Add `ImageLoaderView` component to load in and frame images.

---
+++ SpotifyUIClone/HelperComponents/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SpotifyUIClone
+//
+//  Created by Saverio Negro on 3/29/26.
+//
+
+import SwiftUI
+
+struct Constants {
+    static let randomImageURL = "https://picsum.photos/600/600"
+}
+
---
+++ SpotifyUIClone/HelperComponents/ImageLoader/ImageLoaderView.swift
@@ -0,0 +1,50 @@
+//
+//  ImageLoaderView.swift
+//  SpotifyUIClone
+//
+//  Created by Saverio Negro on 3/29/26.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    
+    private var imageURLStr: String = Constants.randomImageURL
+    private var resizingMode: ContentMode = ContentMode.fill
+    
+    init(imageURLStr: String = Constants.randomImageURL,
+         resizingMode: ContentMode = .fill
+    ) {
+        self.imageURLStr = imageURLStr
+        self.resizingMode = resizingMode
+    }
+    
+    var body: some View {
+        Rectangle()
+            .overlay {
+                WebImage(url: URL(string: imageURLStr))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                
+                // Prevents the view from participating
+                // in hit test operations.
+                
+                // Reason: `WebImage` view pours out
+                // of the boundary of the parent view's
+                // rectangular frame because of the
+                // content mode constraint.
+                    .allowsHitTesting(false)
+            }
+            .clipped()
+            .onTapGesture {
+                print("View Tapped")
+            }
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+        .padding(50)
+}
