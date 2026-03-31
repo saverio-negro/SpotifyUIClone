a3c40ae8be575632dec76eca8cb31d1bda14aad6
saverio-negro <saverionegro@gmail.com>
2026-03-31 18:13:17 +0200

    Rename Spotify Colors

---
+++ SpotifyUIClone/Views/CoreViews/SpotifyHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifyHomeView.swift
+//  SpotifyUIClone
+//
+//  Created by Saverio Negro on 3/31/26.
+//
+
+import SwiftUI
+
+struct SpotifyHomeView: View {
+    var body: some View {
+        Text("Spotify Home View")
+    }
+}
+
+#Preview {
+    SpotifyHomeView()
+}
---
+++ SpotifyUIClone/Views/RootViews/AppView.swift
@@ -0,0 +1,19 @@
+//
+//  AppView.swift
+//  SpotifyUIClone
+//
+//  Created by Saverio Negro on 3/31/26.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    AppView()
+}
